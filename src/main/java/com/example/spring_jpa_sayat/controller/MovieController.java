3219ac5522e450a1d1c57ea02ce0215e8d34dcbb
Sayat86 <s.beysembayev@bk.ru>
2024-12-05 22:08:17 +0600

    add 2 method

--- src/main/java/com/example/spring_jpa_sayat/controller/MovieController.java
+++ src/main/java/com/example/spring_jpa_sayat/controller/MovieController.java
@@ -3,6 +3,10 @@ package com.example.spring_jpa_sayat.controller;
 import com.example.spring_jpa_sayat.model.Movie;
 import com.example.spring_jpa_sayat.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,5 +45,30 @@ public class MovieController {
                                                            @RequestParam Double minRating,
                                                            @RequestParam Double maxRating) {
         return movieRepository.findByReleaseYearInAndRatingBetween(years, minRating, maxRating);
+    }
+
+    @GetMapping("filter-by-rating")
+    public List<Movie> findMoviesByRatingBetween(@RequestParam Double minRating,
+                                                 @RequestParam Double maxRating,
+                                                 @RequestParam(defaultValue = "0") int page,
+                                                 @RequestParam(defaultValue = "5") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return movieRepository.findMoviesByRatingBetween(minRating, maxRating, pageable).getContent();
+    }
+
+    @GetMapping("filter-by-rating-year")
+    public List<Movie> findMoviesByRatingBetweenAndReleaseYearIsBetween(@RequestParam Double minRating,
+                                                                        @RequestParam Double maxRating,
+                                                                        @RequestParam int startYear,
+                                                                        @RequestParam int endYear,
+                                                                        @RequestParam(defaultValue = "releaseYear") String sortBy,
+                                                                        @RequestParam String sortOrder,
+                                                                        @RequestParam(defaultValue = "0") int page,
+                                                                        @RequestParam(defaultValue = "5") int size) {
+        Sort.Direction direction = sortOrder.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return movieRepository.findMoviesByRatingBetweenAndReleaseYearIsBetween(
+                minRating, maxRating, startYear, endYear, pageable).getContent();
     }
 }
--- src/main/java/com/example/spring_jpa_sayat/repository/MovieRepository.java
+++ src/main/java/com/example/spring_jpa_sayat/repository/MovieRepository.java
@@ -1,6 +1,8 @@
 package com.example.spring_jpa_sayat.repository;
 
 import com.example.spring_jpa_sayat.model.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -15,4 +17,9 @@ public interface MovieRepository extends JpaRepository<Movie, Integer> {
     List<Movie> findByReleaseYearBetween(int beforeDate, int afterDate);
 
     List<Movie> findByReleaseYearInAndRatingBetween(List<Integer> years, Double minRating, Double maxRating);
+
+    Page<Movie> findMoviesByRatingBetween(Double minRating, Double maxRating, Pageable pageable);
+
+    Page<Movie> findMoviesByRatingBetweenAndReleaseYearIsBetween(Double minRating, Double maxRating, int startYear,
+                                                                 int endYear, Pageable pageable);
 }
